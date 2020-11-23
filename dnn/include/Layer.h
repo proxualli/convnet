bfed7450b3133d4ce93ab076153d5bb11b0c6047
zamir1001 <dhaene.filip@skynet.be>
2020-11-23 20:54:41 +0100

    latest build

--- dnn/include/Layer.h
+++ dnn/include/Layer.h
@@ -923,7 +923,7 @@ namespace dnn
 					auto max = 2 * std::abs(WeightsScale);
 					std::generate_n(weights.begin(), WeightCount, [&]()
 					{
-						Float value = 0;
+						Float value = Float(0);
 						do { value = distribution(RandomEngine); } while ((value < -max) || (value > max));
 						return value;
 					});
@@ -1036,7 +1036,7 @@ namespace dnn
 					auto max = 2 * std::abs(BiasesScale);
 					std::generate_n(Biases.begin(), BiasCount, [&]()
 					{
-						Float value = 0;
+						Float value = Float(0);
 						do { value = distribution(RandomEngine); } while ((value < -max) || (value > max));
 						return value;
 					});
