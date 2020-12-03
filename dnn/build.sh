84fdb411322892d311ad88a1027301153fe032cb
zamir1001 <dhaene.filip@skynet.be>
2020-12-03 09:48:29 +0100

    latest build

--- dnn/build.sh
+++ dnn/build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 sudo apt-get install nasm
 sudo apt-get install doxygen
 sudo apt-get install cmake
@@ -6,14 +6,11 @@ sudo apt-get install ninja-build
 
 export CC=clang and export CXX=clang++
 export KMP_AFFINITY=granularity=fine,compact,1,0
-export vCPUs=`cat /proc/cpuinfo | grep processor | wc -l`
-export OMP_NUM_THREADS=$((vCPUs / 2))
 export OMP_DISPLAY_ENV=TRUE
 export KMP_SETTINGS=TRUE
+export KMP_BLOCKTIME=0
+export vCPUs=`cat /proc/cpuinfo | grep processor | wc -l`
+export OMP_NUM_THREADS=$((vCPUs / 2))
 
 mkdir -p build && cd build && cmake -G Ninja ..
-
-cp ~/dnn/deps/zlib-1.2.11/zlib.h ~/dnn/build/deps/zlib-1.2.11
-cp ~/dnn/deps/libpng/scripts/pnglibconf.h.prebuilt ~/dnn/build/deps/libpng/pnglibconf.h
-
 ninja
