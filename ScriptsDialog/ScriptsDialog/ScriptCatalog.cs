0259fe9dc76f0e16b895144b8dccbf8d5849e26c
zamir1001 <dhaene.filip@skynet.be>
2021-01-23 10:42:02 +0100

    latest build

--- ScriptsDialog/ScriptsDialog/ScriptCatalog.cs
+++ ScriptsDialog/ScriptsDialog/ScriptCatalog.cs
@@ -660,17 +660,17 @@ namespace ScriptsDialog
                                 W *= 2;
 
                                 blocks.Add(
-                                  Convolution(C, In("CC", A), DIV8(W), 1, 1, 1, 1, 0, 0) +
-                                   BatchNormActivation(C + 1, In("C", C), p.Relu, DIV8(W)) +
-                                   DepthwiseConvolution(C + 1, In("B", C + 1), 1, kernel, kernel, 2, 2, pad, pad) +
-                                   BatchNorm(C + 2, In("DC", C + 1)) +
-                                   Convolution(C + 2, In("B", C + 2), DIV8(W), 1, 1, 1, 1, 0, 0) +
-                                   BatchNormActivation(C + 3, In("C", C + 2), p.Relu, DIV8(W)) +
-                                   DepthwiseConvolution(C + 3, In("CC", A), 1, kernel, kernel, 2, 2, pad, pad) +
-                                   BatchNorm(C + 4, In("DC", C + 3)) +
-                                   Convolution(C + 4, In("B", C + 4), DIV8(W), 1, 1, 1, 1, 0, 0) +
-                                   BatchNormActivation(C + 5, In("C", C + 4), p.Relu, DIV8(W)) +
-                                   Concat(A + 1, In("B", C + 5) + "," + In("B", C + 3)));
+                                    Convolution(C, In("CC", A), DIV8(W), 1, 1, 1, 1, 0, 0) +
+                                    BatchNormActivation(C + 1, In("C", C), p.Relu, DIV8(W)) +
+                                    DepthwiseConvolution(C + 1, In("B", C + 1), 1, kernel, kernel, 2, 2, pad, pad) +
+                                    BatchNorm(C + 2, In("DC", C + 1)) +
+                                    Convolution(C + 2, In("B", C + 2), DIV8(W), 1, 1, 1, 1, 0, 0) +
+                                    BatchNormActivation(C + 3, In("C", C + 2), p.Relu, DIV8(W)) +
+                                    DepthwiseConvolution(C + 3, In("CC", A), 1, kernel, kernel, 2, 2, pad, pad) +
+                                    BatchNorm(C + 4, In("DC", C + 3)) +
+                                    Convolution(C + 4, In("B", C + 4), DIV8(W), 1, 1, 1, 1, 0, 0) +
+                                    BatchNormActivation(C + 5, In("C", C + 4), p.Relu, DIV8(W)) +
+                                    Concat(A + 1, In("B", C + 5) + "," + In("B", C + 3)));
 
                                 A++; C += 5;
                             }
