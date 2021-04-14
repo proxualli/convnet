13d887e0c9fad6a980b047b110ddc745c2c187eb
zamir1001 <dhaene.filip@skynet.be>
2021-04-14 04:40:40 +0200

    latest build

--- Convnet/Dialogs/TrainParameters.xaml.cs
+++ Convnet/Dialogs/TrainParameters.xaml.cs
@@ -41,83 +41,6 @@ namespace Convnet.Dialogs
             textBoxGoToEpoch.Text = Properties.Settings.Default.GoToEpoch.ToString();
             textBoxColorAngle.IsEnabled = Rate.ColorCast > 0;
 
-            /*
-            switch (comboBoOptimizer.SelectedIndex)
-            {
-                case 6: // DNNOptimizers.SGD:
-                    {
-                        textBlockL2penalty.Opacity = 1;
-                        textBoxL2penalty.IsEnabled = true;
-                        textBlockMomentum.Opacity = 0.5;
-                        textBoxMomentum.IsEnabled = false;
-                        textBoxBeta1.IsEnabled = false;
-                        textBoxBeta2.IsEnabled = false;
-                    }
-                    break;
-
-                case 0: // DNNOptimizers.AdaDelta:
-                case 2: // DNNOptimizers.Adam:
-                case 3: // DNNOptimizers.Adamax:
-                case 5: // DNNOptimizers.RMSProp:
-                    {
-                        textBlockL2penalty.Opacity = 0.5;
-                        textBoxL2penalty.IsEnabled = false;
-                        textBlockMomentum.Opacity = 1;
-                        textBoxMomentum.IsEnabled = true;
-                    }
-                    break;
-                case 1: // DNNOptimizers.AdaGrad:
-                    {
-                        textBlockL2penalty.Opacity = 0.5;
-                        textBoxL2penalty.IsEnabled = false;
-                        textBlockMomentum.Opacity = 0.5;
-                        textBoxMomentum.IsEnabled = false;
-                    }
-                    break;
-
-                case 4: // DNNOptimizers.NAG:
-                case 7: // DNNOptimizers.SGDMomentum:
-                case 8: // DNNOptimizers.RAdam:
-                    {
-                        textBlockL2penalty.Opacity = 1;
-                        textBoxL2penalty.IsEnabled = true;
-                        textBlockMomentum.Opacity = 1;
-                        textBoxMomentum.IsEnabled = true;
-                    }
-                    break;
-            }
-
-            switch (comboBoOptimizer.SelectedIndex)
-            {
-                case 0: // DNNOptimizers.AdaDelta:
-                case 1: // DNNOptimizers.AdaGrad:
-                case 4: // DNNOptimizers.NAG:
-                case 6: // DNNOptimizers.SGD:
-                case 7: // DNNOptimizers.SGDMomentum:
-                {
-                    textBoxBeta1.IsEnabled = false;
-                    textBoxBeta2.IsEnabled = false;
-                }
-                break;
-
-                
-                case 2: // DNNOptimizers.Adam:
-                case 3: // DNNOptimizers.Adamax:
-                case 5: // DNNOptimizers.RMSProp:
-                {
-                    textBoxBeta1.IsEnabled = false;
-                    textBoxBeta2.IsEnabled = true;
-                }
-                break;
-               
-                case 8: // DNNOptimizers.RAdam:
-                {
-                    textBoxBeta1.IsEnabled = true;
-                    textBoxBeta2.IsEnabled = true;
-                }
-                break;
-            }
-            */
             comboBoOptimizer.Focus();
         }
 
@@ -201,6 +124,89 @@ namespace Convnet.Dialogs
         private void TextBoxColorCast_TextChanged(object sender, TextChangedEventArgs e)
         {
             textBoxColorAngle.IsEnabled = (float.TryParse(textBoxColorCast.Text, out float result) && result > 0.0f);
+        }
+
+        private void comboBoOptimizer_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (comboBoOptimizer.SelectedIndex)
+            {
+                case 6: // DNNOptimizers.SGD:
+                    {
+                        textBlockL2penalty.Opacity = 1;
+                        textBoxL2penalty.IsEnabled = true;
+                        textBlockMomentum.Opacity = 0.5;
+                        textBoxMomentum.IsEnabled = false;
+                    }
+                    break;
+
+                case 0: // DNNOptimizers.AdaDelta:
+                case 2: // DNNOptimizers.Adam:
+                case 3: // DNNOptimizers.Adamax:
+                case 5: // DNNOptimizers.RMSProp:
+                    {
+                        textBlockL2penalty.Opacity = 0.5;
+                        textBoxL2penalty.IsEnabled = false;
+                        textBlockMomentum.Opacity = 1;
+                        textBoxMomentum.IsEnabled = true;
+                    }
+                    break;
+                case 1: // DNNOptimizers.AdaGrad:
+                    {
+                        textBlockL2penalty.Opacity = 0.5;
+                        textBoxL2penalty.IsEnabled = false;
+                        textBlockMomentum.Opacity = 0.5;
+                        textBoxMomentum.IsEnabled = false;
+                    }
+                    break;
+
+                case 4: // DNNOptimizers.NAG:
+                case 7: // DNNOptimizers.SGDMomentum:
+                case 8: // DNNOptimizers.RAdam:
+                    {
+                        textBlockL2penalty.Opacity = 1;
+                        textBoxL2penalty.IsEnabled = true;
+                        textBlockMomentum.Opacity = 1;
+                        textBoxMomentum.IsEnabled = true;
+                    }
+                    break;
+            }
+
+            switch (comboBoOptimizer.SelectedIndex)
+            {
+                case 0: // DNNOptimizers.AdaDelta:
+                case 1: // DNNOptimizers.AdaGrad:
+                case 4: // DNNOptimizers.NAG:
+                case 6: // DNNOptimizers.SGD:
+                case 7: // DNNOptimizers.SGDMomentum:
+                    {
+                        textBlockBeta1.Opacity = 0.5;
+                        textBoxBeta1.IsEnabled = false;
+                        textBlockBeta2.Opacity = 0.5;
+                        textBoxBeta2.IsEnabled = false;
+                    }
+                    break;
+
+
+                case 2: // DNNOptimizers.Adam:
+                case 3: // DNNOptimizers.Adamax:
+                case 5: // DNNOptimizers.RMSProp:
+                    {
+                        textBlockBeta1.Opacity = 0.5;
+                        textBoxBeta1.IsEnabled = false;
+                        textBlockBeta2.Opacity = 1;
+                        textBoxBeta2.IsEnabled = true;
+                    }
+                    break;
+
+                case 8: // DNNOptimizers.RAdam:
+                    {
+                        textBlockBeta1.Opacity = 1;
+                        textBoxBeta1.IsEnabled = true;
+                        textBlockBeta2.Opacity = 1;
+                        textBoxBeta2.IsEnabled = true;
+                    }
+                    break;
+            }
         }
     }
 }
