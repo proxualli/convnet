8c871f44e9fa1e4470a13b2ffc57742819ba9d9b
zamir1001 <dhaene.filip@skynet.be>
2021-04-22 20:27:13 +0200

    latest build

--- Convnet/PageViewModels/EditPageViewModel.cs
+++ Convnet/PageViewModels/EditPageViewModel.cs
@@ -598,8 +598,11 @@ namespace Convnet.PageViewModels
                     dirty = false;
                 else
                 {
+                    Xceed.Wpf.Toolkit.MessageBox.Show(File.ReadAllText(fileInfo.FullName), "Compiler Result", MessageBoxButton.OK);
+
                     try
                     {
+                        /*
                         var ProcStartInfo = new ProcessStartInfo()
                         {
                             FileName = "dotnet",
@@ -620,12 +623,13 @@ namespace Convnet.PageViewModels
                             dirty = false;
 
                         process.Close();
+                        */
 
                         fileInfo.Delete();
                     }
-                    catch (Exception ex)
+                    catch (Exception/* ex*/)
                     {
-                        Xceed.Wpf.Toolkit.MessageBox.Show(ex.Message, "Start dotnet build process failed", MessageBoxButton.OK);
+                        // Xceed.Wpf.Toolkit.MessageBox.Show(ex.Message, "Start dotnet build process failed", MessageBoxButton.OK);
                     }
 
                     File.Delete(ScriptPath + @"ScriptsDialog.deps.json");
