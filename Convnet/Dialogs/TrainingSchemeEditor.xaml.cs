2348b8f8ae9533911084f42c1b0ee6e94d430c6e
zamir1001 <dhaene.filip@skynet.be>
2021-04-23 18:41:47 +0200

    latest build

--- Convnet/Dialogs/TrainingSchemeEditor.xaml.cs
+++ Convnet/Dialogs/TrainingSchemeEditor.xaml.cs
@@ -4,7 +4,6 @@ using dnncore;
 using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Forms;
 using System.Windows.Input;
 using Cursors = System.Windows.Input.Cursors;
 
