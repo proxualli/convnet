38af3fa44d58fdeea4671e3fcc8ee3daade708e7
zamir1001 <dhaene.filip@skynet.be>
2021-05-09 23:48:50 +0200

    latest build

--- ScriptsDialog/ScriptsDialog/App.xaml.cs
+++ ScriptsDialog/ScriptsDialog/App.xaml.cs
@@ -2,6 +2,8 @@
 using System;
 using System.ComponentModel;
 using System.Globalization;
+using System.Reflection;
+using System.Threading;
 using System.Windows;
 using System.Windows.Markup;
 
@@ -12,10 +14,13 @@ namespace ScriptsDialog
     /// </summary>
     public partial class App : Application, IDisposable
     {
-        MainWindow mainWindow;
+        static readonly SingleInstanceMutex sim;
+        public MainWindow mainWindow;
 
         static App()
         {
+            sim = new SingleInstanceMutex();
+
             FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
             FrameworkContentElement.LanguageProperty.OverrideMetadata(typeof(System.Windows.Documents.TextElement), new FrameworkPropertyMetadata(XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
         }
@@ -29,16 +34,26 @@ namespace ScriptsDialog
 
         protected override void OnStartup(StartupEventArgs e)
         {
+            if (sim.IsOtherInstanceRunning)
+            {
+                Application.Current.Shutdown();
+                return;
+            }
+
             base.OnStartup(e);
 
             try
             {
+                base.OnStartup(e);
+
                 mainWindow = new MainWindow();
                 mainWindow.Closing += MainWindow_Closing;
                 mainWindow.Show();
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                MessageBox.Show(ex.Message);
+                Dispose();
             }
         }
 
@@ -65,5 +80,106 @@ namespace ScriptsDialog
             // Ensure that the destructor is not called
             GC.SuppressFinalize(this);
         }
+    }
+
+    /// <summary>
+    /// Represents a <see cref="SingleInstanceMutex"/> class.
+    /// </summary>
+    public partial class SingleInstanceMutex : IDisposable
+    {
+        #region Fields
+
+        /// <summary>
+        /// Indicator whether another instance of this application is running or not.
+        /// </summary>
+        private bool isNoOtherInstanceRunning;
+
+        /// <summary>
+        /// The <see cref="Mutex"/> used to ask for other instances of this application.
+        /// </summary>
+        private Mutex singleInstanceMutex = null;
+
+        /// <summary>
+        /// An indicator whether this object is beeing actively disposed or not.
+        /// </summary>
+        private bool disposed;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SingleInstanceMutex"/> class.
+        /// </summary>
+        public SingleInstanceMutex()
+        {
+            this.singleInstanceMutex = new Mutex(true, Assembly.GetCallingAssembly().FullName, out this.isNoOtherInstanceRunning);
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets an indicator whether another instance of the application is running or not.
+        /// </summary>
+        public bool IsOtherInstanceRunning
+        {
+            get
+            {
+                return !this.isNoOtherInstanceRunning;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Closes the <see cref="SingleInstanceMutex"/>.
+        /// </summary>
+        public void Close()
+        {
+            this.ThrowIfDisposed();
+            this.singleInstanceMutex.Close();
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                /* Release unmanaged ressources */
+
+                if (disposing)
+                {
+                    /* Release managed ressources */
+                    this.Close();
+                }
+
+                this.disposed = true;
+            }
+        }
+
+        /// <summary>
+        /// Throws an exception if something is tried to be done with an already disposed object.
+        /// </summary>
+        /// <remarks>
+        /// All public methods of the class must first call this.
+        /// </remarks>
+        public void ThrowIfDisposed()
+        {
+            if (this.disposed)
+            {
+                throw new ObjectDisposedException(this.GetType().Name);
+            }
+        }
+
+        #endregion
     }
 }
