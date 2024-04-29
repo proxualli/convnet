a719c7bf3d16f71f0596b94e78ffdc33b7e91618
proxualli <dhaene.filip@skynet.be>
2024-04-29 02:03:56 +0200

    latest build

--- dnn/include/ParallelFor.h
+++ dnn/include/ParallelFor.h
@@ -151,12 +151,9 @@ namespace dnn
 				f(i);
 		}
 	#else
-		PRAGMA_OMP_PARALLEL_THREADS(omp_get_max_threads())
-		{
-			PRAGMA_OMP_FOR_SCHEDULE_STATIC(1)
-			for (auto i = 0ull; i < range; i++)
-				f(i);
-		}
+		#pragma omp parallel for schedule(static,1) num_threads(omp_get_max_threads())
+		for (auto i = 0ull; i < range; i++)
+			f(i);
 	#endif
 	
 #else
@@ -239,9 +236,9 @@ namespace dnn
 					f(i);
 			}
 	#else
-				#pragma omp parallel for schedule(dynamic,1) num_threads(threads)
-				for (auto i = 0ull; i < range; i++)
-					f(i);
+		#pragma omp parallel for schedule(dynamic,1) num_threads(threads)
+		for (auto i = 0ull; i < range; i++)
+			f(i);
 	#endif
 #else
 			DNN_UNREF_PAR(threads);
