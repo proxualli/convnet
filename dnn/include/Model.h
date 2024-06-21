658ebaab46470c94908c5c13dba36c6e4e2480b8
proxualli <dhaene.filip@skynet.be>
2024-06-21 03:27:39 +0200

    latest build

--- dnn/include/Model.h
+++ dnn/include/Model.h
@@ -2438,7 +2438,7 @@ namespace dnn
 
 				if (State.load() == States::Training && idx < DataProv->TrainSamplesCount)
 				{
-					label = DataProv->TrainLabels[RandomTrainSamples[idx]].data();
+					label[LabelIndex] = DataProv->TrainLabels[RandomTrainSamples[idx]][LabelIndex];
 					
 					for (auto i = 0ull; i < size; i++)
 						snapshot[i] = Layers[0]->Neurons[i + offset];
@@ -2447,7 +2447,7 @@ namespace dnn
 				}
 				else if (State.load() == States::Testing && idx < DataProv->TestSamplesCount)
 				{
-					label = DataProv->TestLabels[idx].data();
+					label[LabelIndex] = DataProv->TestLabels[idx][LabelIndex];
 					
 					for (auto i = 0ull; i < size; i++)
 						snapshot[i] = Layers[0]->Neurons[i + offset];
